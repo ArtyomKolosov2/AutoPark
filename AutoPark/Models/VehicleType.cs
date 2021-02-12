4263af33fb72f3ea4743e71c23930f598f2ef9f5
ArtyomKolosov2 <ArtyomKolosov2@users.noreply.github.com>
2021-02-12 19:01:44 +0300

    Add project files.

---
+++ AutoPark/Models/VehicleType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoPark.Models
+{
+    public class VehicleType
+    {
+        public string TypeName { get; set; }
+        public double TaxCoef { get; set; }
+        public VehicleType()
+        {
+        }
+        public VehicleType(string typeName, double taxCoef)
+        {
+            TypeName = typeName;
+            TaxCoef = taxCoef;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(
+                $"name = {TypeName}\n" +
+                $"tax = {TaxCoef}");
+        }
+        public override string ToString() => $"{TypeName},{TaxCoef}";
+
+
+    }
+}
---
+++ AutoPark/Program.cs
@@ -0,0 +1,32 @@
+﻿using AutoPark.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutoPark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carList = new List<VehicleType>()
+            {
+                new VehicleType("Bus", 1.2),
+                new VehicleType("Car", 1),
+                new VehicleType("Rink", 1.5),
+                new VehicleType("Tractor", 1.2),
+            };
+            foreach (var car in carList)
+            {
+                car.Display();
+            }
+            carList.Last().TaxCoef = 1.3;
+            var maxCoef = carList.Max(car => car.TaxCoef);
+            Console.WriteLine(maxCoef);
+            foreach (var car in carList)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
